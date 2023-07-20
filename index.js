e1455c0af0308ff3639e2b50bf5a537005071cc6
Bria222 <briantechologies.co.ke@gmail.com>
2023-07-20 12:36:27 +0300

    add middleware

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const server = http.createServer(app)
 const { API_PORT } = process.env
 const port = process.env.PORT || API_PORT
 
-// server listening
 server.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
